ec184e42503327990c444ea73c227df588b6f4a5
tonylitianyu <tonylitianyu@gmail.com>
2021-10-21 10:18:41 -0500

    clean up repo

--- Final Project Experiment/tracking_usb.py
+++
@@ -1,5 +0,0 @@
-from goprocam import GoProCamera, constants
-gopro = GoProCamera.GoPro(ip_address="172.28.128.51") # change
-gopro.startWebcam(constants.Webcam.Resolution.R720p)
-gopro.webcamFOV(constants.Webcam.FOV.Linear)
-gopro.getWebcamPreview()
